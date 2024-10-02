d6bb3d8f8068ea22195140e24eb1ad05b526ae37
joullutfi <joullutfi76@outlook.com>
2024-10-02 11:22:31 +0200

    contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,6 +8,6 @@ class ContactController extends Controller
 {
     public function show()
     {
-        return view('pages/contact'); // Geeft de contactpagina weer
+        return view('pages.contact'); // Geeft de contactpagina weer
     }
 }
